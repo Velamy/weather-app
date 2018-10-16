727fe341e8be431a44a73817d036e307776943e8
Velamy <velamypm94@gmail.com>
2018-10-16 12:26:43 -0500

    versión 0.0.4.8 Instalación y utilización de materials-ui adaptación del componente weatherLocation para mandarle la Ciudad como Propiedad y obtener datos reales del servidor

--- weather-app/src/components/WeatherLocation/index.js
+++ weather-app/src/components/WeatherLocation/index.js
@@ -1,59 +1,54 @@
 import React, {Component} from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import PropTypes from 'prop-types';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import transformWeather from './../../services/transformWeather';
 import './styles.css';
-import {
-  SUN
-} from './../../constants/weathers';
-import {api_weather} from './../../constants/api_url';
+import getUrlWeatherByCity from './../../services/getUrlWeatherByCity';
 
-const data = {
-  temperature: 20,
-  weatherState: SUN,
-  humidity: 8,
-  wind: "10m/s",
-}
 
 class WeatherLocation extends Component{
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
+    const { city } = props;
     this.state = {
-      city: "Buenos Aires",
-      data: data,
+      city,
+      data: null,
     }
-    console.log("constructor");
   }
   componentDidMount(){
-    console.log("ComponentDidMount")
-    this.handleUpdateClick();
+  this.handleUpdateClick();
   }
   componentDidUpdate(prevProps, prevState){
-    console.log("ComponentDidUpdate")
+
   }
   handleUpdateClick = () => {
       console.log("Actualizado");
+      const api_weather = getUrlWeatherByCity(this.state.city);
       fetch(api_weather).then(response => { //El response es la variable que guarda las cabeceras por parte del servidor
         return response.json();
       }).then(data => { //data es la variable que ya contiene en Json los datos que necesitamos
         const newWeather = transformWeather(data);
-        console.log("Resultado de HandleUpdateClick");
+        console.log(newWeather);
         this.setState({
           data: newWeather
         });
       });
   }
   render(){
-    console.log("render");
     const {city, data} = this.state;
     return (
     <div className="weatherLocationCont">
       <Location city={city}/>
-      <WeatherData data={data}/>
-      <button onClick={this.handleUpdateClick}>Actualizar</button>
+      {data ? <WeatherData data={data}/> :
+              <CircularProgress size={50}/>}
     </div>
     )
   }
 };
+WeatherLocation.propTypes = {
+  city: PropTypes.string.isRequired,
+}
 
 export default WeatherLocation;
--- weather-app/src/constants/api_url.js
+++ weather-app/src/constants/api_url.js
@@ -1,4 +1,3 @@
-const location = "Buenos Aires,ar";
-const api_key ="a63e26a166319df7ac52ef3ca6a9353a";
-const url_base_weather = "http://api.openweathermap.org/data/2.5/weather";
-export const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
+
+export const api_key ="a63e26a166319df7ac52ef3ca6a9353a";
+export const url_base_weather = "http://api.openweathermap.org/data/2.5/weather";
